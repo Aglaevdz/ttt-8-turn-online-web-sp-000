4332ae33b3db6e1ac8b60d4af291ba47509288f3
Aglae Verduzco <aglae.cv@gmail.com>
2018-10-24 00:20:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,11 +3,10 @@ require "pry"
   puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-
     if valid_move?(board,index)
     move(board,index)
     display_board(board)
-   else
+    else
      turn(board)
     end
 end
