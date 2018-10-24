a8ceedaa07c7540aa0910c5c87d199ac4c921508
Aglae Verduzco <aglae.cv@gmail.com>
2018-10-24 00:05:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@ require "pry"
  def turn(board)
   puts "Please enter 1-9:"
     input = gets.strip
-    index = input_to_index(input)
+    input_to_index(input)
 
     while valid_move?(board,index) && !position_taken?(board,index)
     move(board,index)
